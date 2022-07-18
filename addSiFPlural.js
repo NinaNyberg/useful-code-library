e998f7c67c7d5122e92345cecf2ff7b9c02ff4f1
Nina Nyberg <nina.b.nyberg@gmail.com>
2022-07-18 23:42:46 +0300

    Code added

--- addSiFPlural.js
+++ addSiFPlural.js
@@ -1,3 +1,7 @@
-// a tyne line of code to conditionally add an "s" at the end of the word if it is plural
+// a tiny line of code to conditionally add an "s" at the end of the word if it is plural
 
-example.age !== 1 ? 's' : '';
+{
+  <>
+    {example.age} Year{example.age !== 1 ? 's' : ''}{' '}
+  </>;
+}
